a7864905b19b58f53dc7884e90c627ff84fe70b0
simanto97 <saimumsimanto97@gmail.com>
2023-05-03 13:06:49 +0600

    recipe card added for each chef

--- src/Pages/Recipes/RecipeCard/RecipeCard.jsx
+++ src/Pages/Recipes/RecipeCard/RecipeCard.jsx
@@ -1,10 +1,40 @@
 import React from "react";
+import { Card } from "react-bootstrap";
+import { FaHeart, FaRegStar, FaStar } from "react-icons/fa";
+import Rating from "react-rating";
 
-const RecipeCard = () => {
+const RecipeCard = ({ recipe }) => {
   return (
-    <div>
-      <h2>recipe Card section</h2>
-    </div>
+    <Card style={{ width: "18rem" }}>
+      <Card.Img
+        style={{ height: "12rem" }}
+        variant="top"
+        src={recipe.recipe_img}
+      />
+      <Card.Header className="d-flex justify-content-between">
+        <div>
+          <Rating
+            className="text-warning"
+            placeholderRating={recipe.rating}
+            emptySymbol={<FaRegStar></FaRegStar>}
+            placeholderSymbol={<FaStar></FaStar>}
+            fullSymbol={<FaStar></FaStar>}
+          />
+          <span className="ms-2">{recipe.rating}</span>
+        </div>
+        <FaHeart></FaHeart>
+      </Card.Header>
+      <Card.Body>
+        <Card.Title>{recipe.name}</Card.Title>
+        <Card.Text>{recipe.method}</Card.Text>
+        <Card.Text>
+          <h6>Ingredients: </h6>
+          {recipe.ingredients.map((n) => (
+            <li>{n}</li>
+          ))}
+        </Card.Text>
+      </Card.Body>
+    </Card>
   );
 };
 
--- src/Pages/Recipes/Recipes/Recipes.jsx
+++ src/Pages/Recipes/Recipes/Recipes.jsx
@@ -1,17 +1,34 @@
 import React, { useEffect, useState } from "react";
-import { Container } from "react-bootstrap";
+import { Card, CardGroup, Container } from "react-bootstrap";
 import { useLoaderData } from "react-router-dom";
+import RecipeCard from "../RecipeCard/RecipeCard";
 
 const Recipes = () => {
   const chef = useLoaderData();
   const { name, picture, years_of_experience, likes, num_recipes } = chef;
+  const chefRecipes = chef.recipes;
   return (
     <Container>
-      <div>
-        <img src={picture} alt="" />
+      <div className="d-flex align-items-center justify-content-center gap-4">
+        <div>
+          <img
+            style={{ width: "25rem", height: "18rem" }}
+            src={picture}
+            alt=""
+          />
+        </div>
+        <div>
+          <h3 className="text-success">{name}</h3>
+          <p></p>
+          <h6>Experience: {years_of_experience} years</h6>
+          <h6>Recipes: {num_recipes}</h6>
+          <h6>Likes: {likes}</h6>
+        </div>
       </div>
-      <div>
-        <h3></h3>
+      <div className="d-flex justify-content-center">
+        {chefRecipes.map((recipe) => (
+          <RecipeCard key={recipe._id} recipe={recipe}></RecipeCard>
+        ))}
       </div>
     </Container>
   );
